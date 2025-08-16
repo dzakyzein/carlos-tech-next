84d72688c8512a051b1d697f0e0af5b3f3b42fcc
Zykiria21 <dzakyanwar21@gmail.com>
2025-08-16 16:04:17 +0700

    change name navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/RealNavbar';
 import Hero from '@/components/Hero';
 import AboutSection from '@/components/AboutSection';
 import Services from '@/components/Services';
